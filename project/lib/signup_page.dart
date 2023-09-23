49a7123400c8c4f45521297f0c64d67e5a647952
JPEA97 <112213210+JPEA97@users.noreply.github.com>
2023-09-23 15:44:05 -0600

    Update signup_page.dart

--- project/lib/signup_page.dart
+++ project/lib/signup_page.dart
@@ -6,61 +6,80 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Sign Up'),
-        ),
-        body: Column(children: [
-            Text("SIGN UP", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-
-            SizedBox(height: 10,),
-
-            Text("Create an account", style: TextStyle(color: Colors.grey),),
-
-            SizedBox(height: 20.0),
-
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.grey),
-              ),),
-              SizedBox(height: 10.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Colors.grey),
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+            child: Text(
+              "SIGN UP",
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Create an account",
+            style: TextStyle(color: Colors.grey),
+          ),
+          SizedBox(height: 20.0),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Email',
+              hintStyle: TextStyle(color: Colors.grey),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(0.0),
               ),
             ),
-            SizedBox(height: 10.0),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Confirm password',
-                hintStyle: TextStyle(color: Colors.grey),
+          ),
+          SizedBox(height: 20.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              hintStyle: TextStyle(color: Colors.grey),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(0.0),
               ),
             ),
-             SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                //SIGN UP BUTTON LOGIC
-              },
-              child: Text('Sign Up'),
+          ),
+          SizedBox(height: 20.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Confirm password',
+              hintStyle: TextStyle(color: Colors.grey),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(0.0),
+              ),
             ),
-            SizedBox(height: 10.0),
-            GestureDetector(
-              onTap: () {
-                //NAVIGATOR PUSH TO LOGIN PAGE
-              },
-              child: Text(
-                'Already have an account? Login',
-                style: TextStyle(
-                  color: Colors.blue,
-                  decoration: TextDecoration.underline,
-                ),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+              //SIGN UP BUTTON LOGIC
+            },
+            child: Text('Sign Up'),
+          ),
+          SizedBox(height: 10.0),
+          GestureDetector(
+            onTap: () {
+              //NAVIGATOR PUSH TO LOGIN PAGE
+            },
+            child: Text(
+              'Already have an account? Login',
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
               ),
-              ),
+            ),
+          ),
         ],
-        ),
-      );
+      ),
+    );
   }
 }
