eeb1ab1357566c035e57d150cdd59bddd72aad2a
Gabriel Olvera <gavoxolvera@gmail.com>
2023-11-03 20:32:46 -0600

    Event Handling Scaffold using Bloc

--- project/lib/bloc/estacionamiento_event.dart
+++ project/lib/bloc/estacionamiento_event.dart
@@ -1,4 +1,39 @@
 part of 'estacionamiento_bloc.dart';
 
 @immutable
-sealed class EstacionamientoEvent {}
+sealed class EstacionamientoEvent {} //Clase que tendrá la unidad del objeto que tengas sus características que serán
+
+/* •	id del espacio (identificador único del espacio de estacionamiento)
+•	estado (Ocupado, Reservado, Libre, No disponible)
+•	tipo (Discapacitados, Pequeño, Irregular)
+•	salida más cercana (referencia al identificador de la salida más cercana)
+•	distancia en eje X (distancia en metros hasta la salida vertical más cercana)
+•	distancia en eje Y (distancia en metros desde la parte frontal del estacionamiento hasta la fila donde se encuentra) */
+
+// Eventos que afectaran al widget del Mapa segun la interaccion del usuario con los filtros del mapa
+
+class GetFreeParking
+    extends EstacionamientoEvent {} // Evento que devolera los lugares de estacionamiento con el estado de Libre
+
+class GetNotAvailableParking
+    extends EstacionamientoEvent {} // Evento que devolvera los lugares de estacionmiento con el estado de No Disponible
+
+class GetReservedParking
+    extends EstacionamientoEvent {} // Evento que devolvera los lugares de estacionamiento con estado de Reservado
+
+class GetNearestExit
+    extends EstacionamientoEvent {} // Evento que devolvera el id de un documento de la coleccion de salidas que cumpla con ser la mas cercana
+
+class GetOccupiedParking
+    extends EstacionamientoEvent {} // Evento que devolvera los lugares de estacionamiento con el estado de ocupado
+
+class GetHandicapParking
+    extends EstacionamientoEvent {} // Evento que devuelve los documentos de la coleccion que son para discapacitados
+
+class GetSmallParking
+    extends EstacionamientoEvent {} // Evento que devuelve los espacios de estacionamiento que son del tipo peque
+
+class GetIrregularParking
+    extends EstacionamientoEvent {} // Evento que devuelve los espacios de estacionamiento que son del tipo irregular 
+
+
---
+++ project/lib/bloc/switch_event.dart
@@ -0,0 +1,6 @@
+part of 'switch_bloc.dart';
+
+@immutable
+sealed class SwitchEvent {}
+
+class LoadSwitchState {} // Evento que actualizara los valores de los switches a los correspondientes segun la base de datos. 
